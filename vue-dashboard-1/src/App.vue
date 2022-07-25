1d7d7625adb2d31ef3bf5f65e9702e3f686bd523
Davy Jem <dscastanedav@unal.edu.co>
2022-07-25 00:05:48 -0500

    header creado

---
+++ vue-dashboard-1/src/App.vue
@@ -0,0 +1,74 @@
+
+<template>
+ 
+ 
+  <headerrr>
+    <template v-slot:slot1>
+      <img class=" bg-white-500 p-4 m-2 gap-1" src="./assets/HeaderImages/Trazado1820.png">
+    </template> 
+    <template v-slot:slot2>
+      <img class="bg-white-500 p-4 m-2 gap-1" src="./assets/HeaderImages/Grupo1528.png">      
+    </template>
+    <template v-slot:slot3 >
+      <div class="flex items-center border rounded border-r-slate-300 hover:border-indigo-300 w-64 h-10 mx-10">
+        <img class=" object-scale-down bg-white-500 p-0 m-0 w-128 h-8" src="./assets/HeaderImages/lupa.png">
+        <input class=" object-scale-down w-48 h-8 p-4 m-2" placeholder="Búsqueda" >
+      </div>
+    </template>
+    <template v-slot:slot4>
+      <img class="bg-white-500 p-4 m-2 gap-1" src="./assets/HeaderImages/Trazado1292.png">
+    </template>
+    <template v-slot:slot5>
+      <h2>Ver Sitio</h2>
+    </template>
+    <template v-slot:slot6>
+       <img class="bg-white-500 p-4 m-2 gap-1" src="./assets/HeaderImages/Trazado911.png">
+    </template>
+    <template v-slot:slot7>
+      <img class="bg-white-500 p-4 m-2 gap-1" src="./assets/HeaderImages/christopher-campbell-28567-unsplash.png">
+    </template>
+    <template v-slot:slot8>
+      <div class="flex-col">
+        <h2 class="font-semibold">Andrés Suarez</h2>
+        <h2>Administrador</h2>
+      </div>
+    </template>
+    <template v-slot:slot9>
+      <img class="bg-white-500 p-4 m-2 gap-1 mx-5" src="./assets/HeaderImages/Trazado914.png">
+    </template>
+
+  </headerrr>
+
+  
+
+</template>
+
+<script>
+/* eslint-disable vue/no-unused-components */
+
+import headerrr from './components/headerrr.vue'
+ 
+export default {
+  name: 'App',
+  components: {
+    headerrr
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#wrapper {
+  max-width: 940px;
+  margin: 0 auto;
+  padding: 0 5%;
+  clear: both;
+}
+</style>
---
+++ vue-dashboard-1/src/components/headerrr.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex border border-transparent border-b-slate-300 items-center -mt-12">
+        <div id="div1">
+            <slot name="slot1"></slot>
+        </div>
+        <div id="div2">
+            <slot name="slot2"></slot>
+        </div>
+        <div id="div3">
+            <slot name="slot3"></slot>
+        </div>
+        <div id="div4">
+            <slot name="slot4"></slot>
+        </div>
+        <div id="div5">
+            <slot name="slot5"></slot>
+        </div>
+        <div id="div6">
+            <slot name="slot6"></slot>
+        </div>
+        <div id="div7">
+            <slot name="slot7"></slot>
+        </div>
+        <div id="div8">
+            <slot name="slot8"></slot>
+        </div>
+        <div id="div9">
+            <slot name="slot9"></slot>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "my-header"
+    }
+</script>
+
+<style scoped>
+  
+
+    
+
+</style>
